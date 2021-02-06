40d9cbd7f2895aec9d94832a8c9202b528362270
Rebekah <heal.rebekah@gmail.com>
2021-02-06 21:09:01 +1000

    working app

---
+++ main.py
@@ -0,0 +1,32 @@
+import inquirer
+import random
+import os
+from termcolor import colored
+
+def add_option(options_list:list):
+    question = inquirer.prompt([inquirer.Text(name="option", message="Enter an option")])
+    options_list.append(question["option"])
+    os.system("clear")
+    return options_list
+
+options = []
+
+os.system("clear")
+print("Welcome Human.")
+
+option = add_option(options)
+
+print(f"Current options: {options}")
+
+question = inquirer.prompt([inquirer.List(name="choice", choices=["ADD OPTION", "CHOOSE"])])
+
+
+while question["choice"] != "CHOOSE":
+    os.system("clear")
+    print(f"Current options: {options}")
+    option = add_option(options)
+    print(f"Current options: {options}")
+    question = inquirer.prompt([inquirer.List(name="choice", choices=["ADD OPTION", "CHOOSE"])])
+
+os.system("clear")
+print(f"It has been chosen: {colored(random.choice(options), 'red')}")
